4a1c215849308fafbe9486dfdd6e2298c6dc8566
Malgo0102 <mal.wit@hotmail.com>
2020-06-26 20:04:13 +0200

    Working on displaying plants.

--- public/plants/plants.js
+++ public/plants/plants.js
@@ -1,43 +1,36 @@
 
 $(document).ready(function () {
-    $.get('/plants/data')
-        .done((data) => {
+    // $('#plants').DataTable({
+    //     "ajax": {
+    //         "url": "/plants/data",
+    //         "type": "GET",   
+    //         "datatype": 'json', 
+            
+    //     },
 
-            for (var i = 0; i < data.length; i++) {
-                console.log(data);
-                data[i].name;
-                data[i].nickname;
-                data[i].watering;
-                data[i].humidity;
-                data[i].fertalizing;
-                data[i].temperature;
-                data[i].lighting;
-                data[i].soil;
-                data[i].pruning;
-                data[i].picture;
-                data[i].category;
-            }
-
+  
+    $.ajax({
+        "url": "/plants/data",
+        "type": "GET",
+        "datatype": 'json',
+        "success": function (data) {
             $('#plants').DataTable({
                 data: data,
-                columns: [
-                    { data: 'name' },
-                    { data: 'nickname' },
-                    { data: 'watering' },
-                    { data: 'humidity' },
-                    { data: 'fertalizing' },
-                    { data: 'temperature' },
-                    { data: 'lighting' },
-                    { data: 'soil' },
-                    { data: 'pruning' },
-                    { data: 'picture' },
-                    { data: 'category' },
-                ]
-            });
-        })
-        .catch((error) => {
-            console.log(error);
-            $('#plants').text("Couldn't jQuery plants.js");
-        });
+
+        columns: [
+            { data: 'name' },
+            { data: 'nickname' },
+            { data: 'watering' },
+            { data: 'humidity' },
+            { data: 'fertalizing' },
+            { data: 'temperature' },
+            { data: 'lighting' },
+            { data: 'soil' },
+            { data: 'pruning' },
+            { data: 'picture' },
+            { data: 'category' },
+        ]
+    });
+} }) //extra  brackets
 });
 

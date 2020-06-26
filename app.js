cbd61ff291ad8da597c3177e6184f05dd342c764
Malgo0102 <mal.wit@hotmail.com>
2020-06-26 23:17:27 +0200

    Started working on chat with npm socket.io

--- app.js
+++ app.js
@@ -16,6 +16,19 @@ const knex = Knex(knexfile.development);
 Model.knex(knex);
 
 // Setup socket
+const server = require("http").createServer(app);
+const io = require("socket.io")(server);
+
+const escape = require('escape-html');
+
+io.on('connection', socket => {
+
+    socket.on('a client wrote this', (data) => {
+        // emits to all clients
+        io.emit("A client said", { message: escape(data.message) });
+    });
+
+});
 
 // Setup session
 
@@ -32,12 +45,13 @@ const fs = require('fs');
 
 const navPage = fs.readFileSync('./public/fragments/nav.html', 'utf8');
 const footerPage = fs.readFileSync('./public/fragments/footer.html', 'utf8');
+const chatPopup = fs.readFileSync('./public/chat/chat.html', 'utf8');
 
 const homePage = fs.readFileSync('./public/home/home.html', 'utf8');
 const plantsPage = fs.readFileSync('./public/plants/plants.html', 'utf8');
 
 app.get('/', (req, res) => {
-    return res.send(navPage + homePage + footerPage);
+    return res.send(navPage + homePage + chatPopup + footerPage);
 });
 
 app.get('/plants', (req, res) => {

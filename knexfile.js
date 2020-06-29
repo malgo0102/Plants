577b48a9136e5cbc792b5d8d9e9f10681ab2d028
Malgo0102 <mal.wit@hotmail.com>
2020-06-29 22:42:24 +0200

    AWS RDS setup

--- knexfile.js
+++ knexfile.js
@@ -6,9 +6,11 @@ module.exports = {
   development: {
     client: 'mysql',
     connection: {
+      host: credentials.host,
       database: credentials.database,
       user: credentials.user,
       password: credentials.password,
+      port: 3306
     },
     ...knexSnakeCaseMappers()
   }

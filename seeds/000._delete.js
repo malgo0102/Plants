4f93c5ef4116b9d6d8475ef8b7b2de13dce1fa8e
Malgo0102 <mal.wit@hotmail.com>
2020-06-30 16:06:38 +0200

    Corrected seed delete.

--- seeds/000._delete.js
+++ seeds/000._delete.js
@@ -2,10 +2,10 @@ exports.seed = function(knex) {
 
     return knex('users_plants').del()
     .then(function() {
-        return knex('categories').del();
+        return knex('plants').del();
     })
     .then(function() {
-        return knex('plants').del(); 
+        return knex('categories').del(); 
     })
     .then(function () {
         return knex('users').del();

c8b86ee6fad35055372f3da131288e550c3eb274
Malgo0102 <mal.wit@hotmail.com>
2020-08-06 20:27:08 +0200

    Hide/show buttons in the nav if logged in/out.

--- public/fragments/nav.js
+++ public/fragments/nav.js
@@ -1,5 +1,23 @@
+var mysession;
+
 $(document).ready(()=> {
-    
-    //$("#login, #signup").hide();
+    $.get('/session')
+        .done(data => {
+            mysession = data.response;
+            
+            if (mysession.loggedin){
+                $("#login, #signup").hide();
+
+            } else {
+                $("#user, #logout").hide();
+                $(".mycheckbox, .save-plant-btn").hide();
+            }
+            if (mysession.errormessage){
+                $("#errormessage").prepend("<div class='alert alert-info alert-dismissible'><a href='#' class='close' data-dismiss='alert' aria-label='close'>&times;</a><strong>Info! </strong>" + mysession.errormessage + "</div>");
+            }
+
+        }).catch((error) => {
+            console.log(error);
+        });
     
 });

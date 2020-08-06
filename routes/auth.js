c50e483093394377ed10882d964c0cdb4fb34bb8
Malgo0102 <mal.wit@hotmail.com>
2020-08-06 20:36:08 +0200

    Added error messages.

--- routes/auth.js
+++ routes/auth.js
@@ -31,52 +31,64 @@ router.post('/login', (req, res) => {
     if (username && password) {
         // Check if user exists and get their password
         try {
-            User.query().where('username', username).select('username', 'password').then(foundUser => {
-                //console.log(foundUser[0].password);
+            User.query().where('username', username).select('id', 'password').then(foundUser => {
                 if (foundUser.length < 1) {
-                    return res.status(400).send({ response: "User not found" });
+                    req.session.errormessage = "User not found";
+                    //return res.status(400).send({ response: "User not found" });
+                    return res.send(navPage + loginPage + chatPopup + footerPage);
                 } else {
                     bcrypt.compare(password, foundUser[0].password).then((result) => {
-                        console.log("post login - your password:", password)
+                        //console.log("post /login - your password:", password)
 
                         if (result) {
                             // Send a response based on the comparison handle session
                             req.session.loggedin = true;
                             req.session.username = username;
+                            req.session.userid = foundUser[0].id;
                             return res.redirect('/');
                         } else {
-                            return res.status(400).send(
-                                { response: "Password does not match" });
+                            req.session.errormessage = "Password does not match";
+                            //return res.status(400).send({ response: req.session.errormessage });
+                            return res.send(navPage + loginPage + chatPopup + footerPage);
                         }
                     });
                 }
             });
         } catch (error) {
-            return res.status(500).send({ response: "Something went wrong with the DB" });
+            req.session.errormessage = "Something went wrong with the DB";
+            //return res.status(500).send({ response: "Something went wrong with the DB" });
+            return res.send(navPage + loginPage + chatPopup + footerPage);
         }
     } else {
-        return res.status(400).send({ response: "Username or password missing" });
+        req.session.errormessage = "Username or password missing";
+        //return res.status(400).send({ response: "Username or password missing" });
+        return res.send(navPage + loginPage + chatPopup + footerPage);
     }
 });
 
 router.post('/signup', (req, res) => {
     const { username, password } = req.body;
-    console.log("router post signup")
     if (username && password) {
         // password validation
         if (password.length < 8) {
-            return res.status(400).send({ response: "Password must be 8 characters or longer" });
+            req.session.errormessage = "Password must be 8 characters or longer";
+            //return res.status(400).send({ response: "Password must be 8 characters or longer" });
+            return res.send(navPage + signupPage + chatPopup + footerPage);
         } else {
             try {
                 User.query().select('username').where('username', username).then(foundUser => {
                     if (foundUser.length > 0) {
-                        return res.status(400).send({ response: "User already exists" });
+                        req.session.errormessage = "User already exists";
+                        //return res.status(400).send({ response: "User already exists" });
+                        return res.send(navPage + signupPage + chatPopup + footerPage);
                     } else {
                         bcrypt.hash(password, saltRounds).then(hashedPassword => {
                             User.query().insert({
                                 username,
                                 password: hashedPassword
+                                
                             }).then(createdUser => {
+                                req.session.errormessage = "The user " + createdUser.username + " was created";
                                 //return res.send({ response: `The user ${createdUser.username} was created` });
                                 return res.redirect('/login');
                             });
@@ -84,11 +96,14 @@ router.post('/signup', (req, res) => {
                     }
                 });
             } catch (error) {
-                return res.status(500).send({ response: "Something went wrong with the DB" });
+                req.session.errormessage = "Something went wrong with the DB";
+                //return res.status(500).send({ response: "Something went wrong with the DB" });
+                return res.send(navPage + signupPage + chatPopup + footerPage);
             }
         }
     } else {
-        return res.status(400).send({ response: "username or password missing" });
+        req.session.errormessage = "Something went wrong with the DB";
+        return res.send(navPage + signupPage + chatPopup + footerPage);
     }
 });
 

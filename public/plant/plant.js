1afafcb87b12d23f8da00faa627055d06f3c450e
Malgo0102 <mal.wit@hotmail.com>
2020-08-06 20:28:28 +0200

    Redisigned Plant and Plants views.

--- public/plant/plant.js
+++ public/plant/plant.js
@@ -1,32 +1,22 @@
-
 $(document).ready(function () {
-    var url = window.location.href;
-    var pathArray = url.split("/");
-    var id = pathArray[4];
+    const url = window.location.href;
+    let id = url.substr(url.lastIndexOf("/") + 1);
 
-    $.ajax({
-        "url": "/plant/data/" + id,
-        "type": "GET",
-        "datatype": 'json',
-        "success": function (x) {
-            $('#plants').DataTable({
-                data: x.response,
-
-                columns: [
-                    { data: 'name' },
-                    { data: 'nickname' },
-                    { data: 'watering' },
-                    { data: 'humidity' },
-                    { data: 'fertalizing' },
-                    { data: 'temperature' },
-                    { data: 'lighting' },
-                    { data: 'soil' },
-                    { data: 'pruning' },
-                    //{ data: 'picture' },
-                    //{ data: 'category' },
-                ]
-            });
-        }
-    })
+    $.get(`/plant/data/${id}`)
+        .done((data) => {
+            $("#name").text(data.response.name);
+            $("#nickname").text(data.response.nickname);
+            $("#watering").text(data.response.watering);
+            $("#humidity").text(data.response.humidity);
+            $("#fertilizing").text(data.response.fertilizing);
+            $("#temperature").text(data.response.temperature);
+            $("#lighting").text(data.response.lighting);
+            $("#soil").text(data.response.soil);
+            $("#pruning").text(data.response.pruning);
+            $("#picture").prepend("<img id='image' src=" + data.response.picture + "></img>");
+            $("#category").text(data.response.category);
+        }).catch((error) => {
+            console.log(error);
+        });
 });
 
--- public/plants/plants.js
+++ public/plants/plants.js
@@ -1,30 +1,25 @@
+var mysession;
 
 $(document).ready(function () {
-    $.ajax({
-        "url": "/plants/data",
-        "type": "GET",
-        "datatype": 'json',
-        "success": function (x) {
-            $('#plants').DataTable({
-                data: x.response,
 
-                columns: [
-                    { render: function (data, type, row) {
-                        return '<a href="/plant/'+ row.id +'">' + row.name + '</a>'
-                    }},
-                    //{ data: 'name' },
-                    { data: 'nickname' },
-                    // { data: 'watering' },
-                    // { data: 'humidity' },
-                    // { data: 'fertalizing' },
-                    // { data: 'temperature' },
-                    // { data: 'lighting' },
-                    // { data: 'soil' },
-                    // { data: 'pruning' },
-                    // { data: 'picture' },
-                    { data: 'category' },
-                ]
-            });
-        }
-    })
-});
+    $.get(`/plants/data`)
+        .done((data) => {
+            let i;
+            if (mysession.loggedin){
+                for ( i = 0; i < data.response.length; i++) {
+                    $("#plants").append("<div class='row'><div class='col'><p id='name'><a href='/plant/" + data.response[i].id + "'>" + data.response[i].name + "</a></p></div><div class='col'><p id='nickname'>" + data.response[i].nickname + "</p></div><div class='col'><p id='category'>" + data.response[i].name + "</p></div><div class='col save-plant-btn'><button type='submit' class='btn btn-primary save-plant-btn' id='" + data.response[i].id +"'>I have this plant!</button></div></div>");
+                    $("#" + data.response[i].id).click(data.response[i].id, function (event){
+                        console.log(event);
+                        $.post("/plants/" + event.data);
+                    });
+                }
+            } else {
+                for ( i = 0; i < data.response.length; i++) {
+                    $("#plants").append("<div class='row'><div class='col'><p id='name'><a href='/plant/" + data.response[i].id + "'>" + data.response[i].name + "</a></p></div><div class='col'><p id='nickname'>" + data.response[i].nickname + "</p></div><div class='col'><p id='category'>" + data.response[i].name + "</p></div></div>");
+                }
+            }
+        }).catch((error) => {
+            console.log(error);
+        });
+
+}); 
